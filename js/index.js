6f9a3c61abd6458ca04d646b18c711592b0f58d0
yaoxinc <yaoxinc@yonyou.com>
2017-01-18 11:01:56 +0800

    提取内容

---
+++ js/index.js
@@ -0,0 +1,22 @@
+/**
+ * Module : Kero webpack entry index
+ * Author : yaoxinc
+ * Date	  : 2017-01-17 16:51:05
+ */
+import {extend} from 'tinper-sparrow/js/extend';
+
+import {EnableMixin} from './enableMixin';
+import {RequiredMixin} from './requiredMixin';
+import {ValidateMixin} from './validateMixin';
+import {ValueMixin} from './valueMixin';
+
+var ex = {
+	EnableMixin: EnableMixin,
+	RequiredMixin: RequiredMixin,
+	ValidateMixin: ValidateMixin,
+	ValueMixin: ValueMixin
+};
+
+extend(ex,window.u || {});
+window.u = ex;
+export { ex as u };
